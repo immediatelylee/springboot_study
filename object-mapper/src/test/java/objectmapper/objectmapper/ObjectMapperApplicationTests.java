6d27aec726c107eb1a72e344e448c9e9d8c4968d
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-01-11 17:20:25 +0900

    objectmapper

--- object-mapper/src/test/java/objectmapper/objectmapper/ObjectMapperApplicationTests.java
+++ object-mapper/src/test/java/objectmapper/objectmapper/ObjectMapperApplicationTests.java
@@ -1,18 +1,15 @@
 package objectmapper.objectmapper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.tomcat.jni.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ObjectMapperApplicationTests {
-//오브젝트 메퍼란? text형태인 json을 object로
-//    object를 text형태인 json으로 바꿔준다.
-
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException{
         System.out.println("-------");
         // Text Json -> Object
         // Object -> Text Json
@@ -20,18 +17,18 @@ class ObjectMapperApplicationTests {
         // controller req json(text) -> object
         // response object -> json(text)
 
-        var objectMapper = new ObjectMapper();
+        var objectMapper = new ObjectMapper(); // (1) ObjectMapper생성
         // object -> text
         // object mapper get method를 활용한다.
 
-        var user = new User("steve",10,"010-1111-2222"); // User 클래스 무엇으로
-        // 변환할 때 예외가 발생할 수 있기 때문에 alt enter로 첫번째 출력 뭔소리지?
-        var text = objectMapper.writeValueAsString(user);
+        var user = new User("steve",10); //(3) 실습을 위한 Object 생성
+
+        var text = objectMapper.writeValueAsString(user); // (4) writeValueAsString메소드 이용,예외처리하기
         System.out.println(text);
 
         //text -> object
-        var objectUser = objectMapper.readValue(text,User.class);
-        System.out.println(objectUser);
+//        var objectUser = objectMapper.readValue(text,User.class);
+//        System.out.println(objectUser);
 
     }
 
