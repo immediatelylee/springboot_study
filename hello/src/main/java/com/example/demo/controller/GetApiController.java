e3b0540c69e7b61829dc39fd3dd8f056b09e0205
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-04-02 02:03:39 +0900

    Update GetApiController.java

--- hello/src/main/java/com/example/demo/controller/GetApiController.java
+++ hello/src/main/java/com/example/demo/controller/GetApiController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLOutput;
 
 @RestController // 해당 클래스는 REST API 처리를 하는 Controller
 @RequestMapping("/api/get") // RequestMapping URI를 지정해주는 Annotation
@@ -11,5 +14,13 @@ public class GetApiController {
     @GetMapping("/hello")
     public String Hello(){
         return "get hello";
+    }
+
+    @GetMapping("/path-variable/{name}")
+    public String pathVariable(@PathVariable String name){
+
+        System.out.println("PathVariable :" + name);
+        return name;
+
     }
 }
