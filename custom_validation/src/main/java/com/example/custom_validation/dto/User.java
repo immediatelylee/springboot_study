a4c872bcc5b07cf588e44b7e4f46c7b31171acaa
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-04-13 22:19:44 +0900

    custom_validation

---
+++ custom_validation/src/main/java/com/example/custom_validation/dto/User.java
@@ -0,0 +1,50 @@
+package com.example.custom_validation.dto;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.util.List;
+
+public class User {
+
+    @NotBlank
+    private String name;
+
+    @Max(value = 90)
+    private int age;
+
+    @Valid
+    private List<Car> cars;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", cars=" + cars +
+                '}';
+    }
+}
