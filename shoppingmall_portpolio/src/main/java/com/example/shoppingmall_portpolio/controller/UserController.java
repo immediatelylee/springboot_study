20b959193b83a6165d685536e8ff3c8d5606138a
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-05-17 23:13:35 +0900

    usercontroller

--- shoppingmall_portpolio/src/main/java/com/example/shoppingmall_portpolio/controller/UserController.java
+++ shoppingmall_portpolio/src/main/java/com/example/shoppingmall_portpolio/controller/UserController.java
@@ -5,7 +5,10 @@ import com.example.shoppingmall_portpolio.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,6 +22,23 @@ public class UserController {
 
     @GetMapping("/login")
     public String login(HttpServletRequest request, Model model)
+    {
+        String referer = request.getHeader("Referer");
+        request.getSession().setAttribute("prevPage",referer);
+        model.addAtrribute("catMapList",categoryService.getCategoryList());
+
+        return "user/login-register";
+    }
+
+    // 일반유저 회원가입
+    @PostMapping("/member")
+    public String registration(@ModelAttribute @Valid UserRequestDto userRequestDto, RedirectAttributes rttr) {
+        userService.userRegistration(userRequestDto);
+
+        rttr.addFlashAttribute("registerComplete", "회원가입이 완료되었습니다.");
+
+        return "redirect:/login";
+    }
 
 
 
--- shoppingmall_portpolio/src/main/java/com/example/shoppingmall_portpolio/service/CategoryService.java
+++ shoppingmall_portpolio/src/main/java/com/example/shoppingmall_portpolio/service/CategoryService.java
@@ -1,4 +1,16 @@
 package com.example.shoppingmall_portpolio.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
 public class CategoryService {
+
+    private final CategoryRepositroy categoryRepository;
+    private final ValueOperations<String,Object> valueOperations;
+
+
 }
