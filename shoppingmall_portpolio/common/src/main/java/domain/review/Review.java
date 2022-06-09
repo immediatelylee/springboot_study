518432fba9aad6ee476018f70947942cb79ff3fc
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-06-09 23:16:12 +0900

    review-domain,repository

---
+++ shoppingmall_portpolio/common/src/main/java/domain/review/Review.java
@@ -0,0 +1,44 @@
+package domain.review;
+
+import domain.user.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 1000000000)
+    private String content;
+
+    @Column
+    private int rate;
+    @Column
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name="product_id" referencedColumnName = "id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",referencedColumnName = "id")
+    private User user;
+
+    public ReviewResponseDto toResponseDto() {
+        LocalDateTime createdDate = this.getCreatedDate();
+
+        return ReviewResponseDto.builder()
+                .id(id)
+                .userIdentifier(user.toReviewResponseDto())
+                .title(title)
+                .rate(rate)
+                .createdDate(createdDate.getYear() + "-" + createdDate.getMonthValue() + "-"
+                        + createdDate.getDayOfMonth() + " " + createdDate.getHour() + ":" + createdDate.getMinute() + ":"
+                        + createdDate.getSecond())
+                .build();
+    }
+
+}
