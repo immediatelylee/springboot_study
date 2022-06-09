dfc9c6b4a2ec95cf9e38c217db8a6a2734d33567
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-06-09 23:15:56 +0900

    product-domain,repository

---
+++ shoppingmall_portpolio/common/src/main/java/domain/product/ProductRepository.java
@@ -0,0 +1,14 @@
+package domain.product;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public interface ProductRepository {
+
+
+
+}
