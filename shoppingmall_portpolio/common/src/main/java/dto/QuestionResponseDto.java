51d385276100a34f57b1b085cacc97b18727604a
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-06-11 22:34:06 +0900

    Dtos Question,review updatepassword

---
+++ shoppingmall_portpolio/common/src/main/java/dto/QuestionResponseDto.java
@@ -0,0 +1,21 @@
+package dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@ToString
+public class QuestionResponseDto {
+
+    private Long id;
+    private String message;
+    private boolean answerState;
+    private Integer answerCount;
+    private String createdDate;
+    private LocalDateTime modifiedDate;
+    private com.shoppingmall.dto.UserResponseDto.QuestionUserResponseDto questionUserResponseDto;
+}
