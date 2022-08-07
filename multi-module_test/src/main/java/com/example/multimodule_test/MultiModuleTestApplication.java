a40a16e38e2c8d3bee04dd3ee3a5c4586f01a302
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-08-07 22:46:02 +0900

    1

---
+++ multi-module_test/src/main/java/com/example/multimodule_test/MultiModuleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimodule_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleTestApplication.class, args);
+    }
+
+}
